309244c206e10a7672cf6a85ac68d7b66691f0d8
Mgeni-E <e.mgeni@alustudent.com>
2024-09-27 11:04:58 +0200

    Stage two

--- uni_residence_project/resident_api/views.py
+++ uni_residence_project/resident_api/views.py
@@ -6,7 +6,6 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework import viewsets
 
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
